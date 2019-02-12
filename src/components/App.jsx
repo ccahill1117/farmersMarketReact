8a4be23448f79854a0c0fefb5d537f08d85e3d0c
Chris Cahill and Paige Williams <paige@>
2019-02-12 10:42:13 -0800

    Set up environment and create weekday and weekdayCard components.

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Weekday from './Weekday';
+
+
+function App(){
+  return (
+    <div>
+    <Weekday />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WeekdayCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function WeekdayCard(props){
+  return (
+    <div>
+      <h1>{props.day}</h1>
+      <h3>{props.location}</h3>
+      <p>{props.hours}</p>
+      <p>{props.booth}</p>
+    </div>
+  );
+}
+
+WeekdayCard.propTypes = {
+  day: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  hours: PropTypes.string.isRequired,
+  booth: PropTypes.string.isRequired
+};
+
+export default WeekdayCard;
